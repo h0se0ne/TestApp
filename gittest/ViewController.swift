e58af2983aea7ebb969b7c819089f6ca3032407f
Karime Möll <moell.kari@me.com>
2016-06-28 12:30:00 +0200

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by Karime Möll on 28.06.16.
+//  Copyright © 2016 Karime Möll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
